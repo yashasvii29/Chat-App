73a7617edd12dab49de6b3ca6ba30a81c362a623
yashasvi <yashasviagrawal29@gmail.com>
2024-02-20 21:10:26 +0530

    add login and signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Login from './Components/Auth/Login'
+import Register from './Components/Auth/Register'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <BrowserRouter>
+            <Routes>
+              {/* <Route path="/" element={<LandingPage />} />
+              <Route path="/home" element={<Home/>}  / > */}
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/login" element={<Login/>}/>
+              </Routes>
+     </BrowserRouter>
+    </div>
   )
 }
 
