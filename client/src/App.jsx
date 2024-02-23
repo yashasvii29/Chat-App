99b9249f650e60d608bb3b8466e1038d91abc3d6
Tanya <tannusharma.dk@gmail.com>
2024-02-23 14:50:52 +0530

    landing page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Login from './Components/Auth/Login'
 import Register from './Components/Auth/Register'
+import Home from './Components/Landing_Page/Home'
+import Layout from './Components/Landing_Page/Layout'
+import About from './Components/Landing_Page/About'
 
 
 const App = () => {
@@ -9,10 +12,17 @@ const App = () => {
     <div>
         <BrowserRouter>
             <Routes>
-              {/* <Route path="/" element={<LandingPage />} />
-              <Route path="/home" element={<Home/>}  / > */}
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/login" element={<Login/>}/>
+            
+              <Route path='/' element={<Layout/>}> 
+              <Route path='/' element={<Home/>} />
+              <Route path='/about' element={<About/>} />
+              
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>} />
+
+
+              </Route>
+                
               </Routes>
      </BrowserRouter>
     </div>
---
+++ client/src/Components/Landing_Page/Header.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import {Link,NavLink } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <header className="shadow sticky z-50 top-0 ">
+            <nav className="bg-[#e0b1cb] px-4 lg:px-6 py-2.5">
+                <div className="flex  flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <Link to="/" className="flex items-center">
+                        <img
+                            src="https://img.freepik.com/premium-vector/social-network-people-sit-social-networks-their-phones-send-chat-messages_491047-418.jpg?w=996"
+                            className="mr-3 h-14"
+                            alt="Logo"
+                        />
+                        
+                        <h1 className=' pl-4 text-4xl font-bold'>Chatting App</h1>
+                        
+                    </Link>
+                    <div className="flex items-center lg:order-2">
+                        <Link
+                            to="/login"
+                            className="text-gray-800 hover:bg-purple-400 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            to="/register"
+                            className="text-gray-800 hover:bg-purple-400 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Sign Up
+                        </Link>
+                        <Link
+                            to="#"
+                            className="text-white bg-purple-400 hover:bg-purple-400 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Get started
+                        </Link>
+                    </div>
+                    <div
+                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <NavLink
+                                to="/"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text=orange-700":"text-gray-700" } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to="/about"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text=orange-700":"text-gray-700" } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    About
+                                </NavLink>
+                            </li>
+
+
+                            
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+
